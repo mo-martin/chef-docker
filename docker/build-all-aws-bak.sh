a383c028cafb15d6d20dd8604b02566b82f6bf81
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-13 16:52:32 +0100

    added new aws server config

--- docker/build-all-aws-bak.sh
+++ docker/build-all-aws-bak.sh
@@ -7,9 +7,9 @@ docker stop moses-api
 docker rm moses-api
 docker stop moses-db
 docker rm moses-db
-find ~/scripts/docker/my-node-api/api/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
-find ~/scripts/docker/my-node-app/app/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
-find ~/scripts/docker/my-node-db/db/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
+find ~/scripts/chef-docker/docker/my-node-api/api/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
+find ~/scripts/chef-docker/docker/my-node-app/app/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
+find ~/scripts/chef-docker/docker/my-node-db/db/* -type d -not -name '.gitignore' -print0 | xargs -0 rm -rf --
 set -e
 
 rm -rf /tmp/node-api/api
@@ -21,7 +21,7 @@ mkdir -p /tmp/node-db/db
 
 cp -r ~/pokergame/app/. /tmp/node-app/app/
 cp -r ~/pokergame/api/. /tmp/node-api/api/
-cp -r ~/scripts/database/. /tmp/node-db/db/
+cp -r ~/scripts/chef-docker/database/. /tmp/node-db/db/
 rm -rf /tmp/node-app/app/.git
 rm -rf /tmp/node-app/app/.gitignore
 rm -rf /tmp/node-api/api/.git
