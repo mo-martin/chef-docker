0ff56ad3f95aa93ef2933c97656eb8083877789b
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-13 20:58:28 +0100

    app folder not found, fixed.

--- docker/build-all-aws.sh
+++ docker/build-all-aws.sh
@@ -27,21 +27,21 @@ rm -rf ~/pokergame/tmp/node-app/app/.gitignore
 rm -rf ~/pokergame/tmp/node-api/api/.git
 rm -rf ~/pokergame/tmp/node-api/api/.gitignore
 
-cp -r ~/pokergame/tmp/node-app/app/. ~/scripts/docker/my-node-api/app/
+cp -r ~/pokergame/tmp/node-app/app/. ~/scripts/docker/my-node-app/app/
 cp -r ~/pokergame/tmp/node-api/api/. ~/scripts/docker/my-node-api/api/
 cp -r ~/pokergame/tmp/node-db/db/. ~/scripts/docker/my-node-db/db/
 
 
-pushd ./my-node-app
+pushd my-node-app
 docker build --tag my-node-app:latest .
 popd
-pushd ./my-node-api
+pushd my-node-api
 docker build --tag my-node-api:latest .
 popd
-pushd ./my-node-db
+pushd my-node-db
 docker build --tag my-node-db:latest .
 popd
-pushd ./my-node-data
+pushd my-node-data
 docker build --tag my-node-data:latest .
 popd
 
