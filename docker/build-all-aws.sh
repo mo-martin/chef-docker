c5e07847cc190b5df4c9d6cd0c9aae0623dfd0e4
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-13 14:57:56 +0100

    more docker compose refinements + folder rename

--- docker/build-all-aws.sh
+++ docker/build-all-aws.sh
@@ -44,14 +44,14 @@ popd
 pushd my-node-db
 docker build --tag my-node-db:latest .
 popd
-pushd my-mongo-data
-docker build --tag my-mongo-data:latest .
+pushd my-node-data
+docker build --tag my-node-data:latest .
 popd
 
 # Mongo
 echo -e "\033[0;31mChecking for my-mongo-data \033[0m"
 set +e
-docker ps -a | grep my-mongo-data > /dev/null
+docker ps -a | grep my-node-data > /dev/null
 FOUND_MONGO=$?
 set -e
 
@@ -59,10 +59,10 @@ if [[ "$FOUND_MONGO" == "0" ]]; then
  echo -e "\033[0;32mMongo data store already exists\033[0m"
 else
  echo -e "\033[1;33mRunning data container\033[0m"
- docker create --name my-mongo-data my-mongo-data:latest
+ docker create --name my-node-data my-node-data:latest
 fi
 
-docker run --name moses-db --volumes-from my-mongo-data -d my-node-db:latest
+docker run --name moses-db --volumes-from my-node-data -d my-node-db:latest
 docker run --name moses-api -p 3001:3001 --link moses-db:db -e DB_URL=mongodb://db/Poker -d my-node-api:latest
 docker run --name moses-app -p 3000:3000 --link moses-api:api -e API_URL=http://ec2-52-18-216-199.eu-west-1.compute.amazonaws.com:3001 -d my-node-app:latest
 docker ps -a
